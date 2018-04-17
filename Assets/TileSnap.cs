71d054eedc490579924432094da435369c20c27a
Stewart Wilson <stewart.wilson.c@gmail.com>
2018-04-17 10:23:10 -0700

    created grid snapping for tiles

---
+++ Assets/TileSnap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TileSnap : MonoBehaviour {
+
+    public float xSnap;
+    public float ySnap;
+	
+	// Update is called once per frame
+	void Update () {
+        if (xSnap != 0 && ySnap != 0) {
+            foreach (Transform child in transform)
+            {
+                float xPos = child.position.x;
+                float yPos = child.position.y;
+
+                float xMod = xPos % xSnap;
+                if (xMod != 0)
+                {
+                    int rounded = Mathf.RoundToInt(xPos / xSnap);
+                    child.position = new Vector2(rounded * xSnap, yPos);
+                }
+                float yMod = yPos % ySnap;
+                if (yMod != 0)
+                {
+                    int rounded = Mathf.RoundToInt(yPos / ySnap);
+                    child.position = new Vector2(xPos, rounded * ySnap);
+                }
+
+            }
+        }
+    }
+}
